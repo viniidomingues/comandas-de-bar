c15e78a3528c3ac590f163c14525e9214baf04dc
viniidomingues <vinicius4al@gmail.com>
2024-04-25 20:01:13 -0300

    commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+import { LoginComponent } from './components/pages/login/login.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [LoginComponent],
+  template: `<app-login />`,
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'MOISES';
+}
---
+++ src/app/components/pages/login/login.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [LoginComponent],
+  templateUrl: './login.component.html',
+  styles: ``
+})
+export class LoginComponent {
+  
+}
