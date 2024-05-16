601c436f5c268ae9e72bd8af055fb534944a60cb
viniidomingues <vinicius4al@gmail.com>
2024-05-16 19:44:41 -0300

    project-feat:adicionado acao de login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,4 @@ import { LoginComponent } from './components/pages/login/login.component';
   template: `<app-login />`,
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  onLogin(event: any) {
-    // Implemente a lógica de autenticação aqui.
-    console.log('Usuário:', event.username);
-    console.log('Senha:', event.password);
-  }
-}
+export class AppComponent {}
--- src/app/components/pages/login/login.component.ts
+++ src/app/components/pages/login/login.component.ts
@@ -1,34 +1,48 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { StorageService } from '../../../services/user/storage.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [LoginComponent],
+  imports: [LoginComponent, ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styles: ``
+  styles: ``,
 })
 export class LoginComponent {
-  @Output() login = new EventEmitter<any>();
+  signupForm!: FormGroup;
 
-  loginForm: FormGroup;
-
-  constructor(private formBuilder: FormBuilder) {
-    this.loginForm = this.formBuilder.group({
-      username: ['', Validators.required],
-      password: ['', Validators.required]
+  constructor(private storageService: StorageService, private router: Router) {
+    this.signupForm = new FormGroup({
+      email: new FormControl(''),
+      password: new FormControl(''),
     });
   }
 
-  onSubmit() {
-    if (this.loginForm.valid) {
-      this.login.emit(this.loginForm.value);
-      this.loginForm.reset();
-    }
+  get email(): string | any {
+    return this.signupForm.get('email')!;
   }
-  onLogin(event: any) {
-    // Implemente a lógica de autenticação aqui.
-    console.log('Usuário:', event.username);
-    console.log('Senha:', event.password);
+
+  get password(): string | any {
+    return this.signupForm.get('password')!;
+  }
+
+  onSubmit() {
+    if (this.signupForm.valid) {
+      console.log(`entrou`);
+      const user = this.signupForm.value;
+      this.storageService.saveUser(user);
+      this.router.navigate(['/home']);
+      setTimeout(() => {
+        location.reload();
+      }, 1000);
+    }
   }
 }
