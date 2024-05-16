f4089a96cdad63c8c1aa51e7e37487c7f713e22a
viniidomingues <vinicius4al@gmail.com>
2024-05-16 19:48:23 -0300

    project-feat:adicionado acao de login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/pages/home/home.component';
 
 export const routes: Routes = [
-  { path: 'home', title: 'Home', component: ListTasksComponent },
+  { path: 'home', title: 'Home', component: HomeComponent },
 ];
---
+++ src/app/components/pages/home/home.component.ts
@@ -0,0 +1,48 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { StorageService } from '../../../services/user/storage.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [HomeComponent, ReactiveFormsModule],
+  templateUrl: './home.component.html',
+  styles: ``,
+})
+export class HomeComponent {
+  signupForm!: FormGroup;
+
+  constructor(private storageService: StorageService, private router: Router) {
+    this.signupForm = new FormGroup({
+      email: new FormControl(''),
+      password: new FormControl(''),
+    });
+  }
+
+  get email(): string | any {
+    return this.signupForm.get('email')!;
+  }
+
+  get password(): string | any {
+    return this.signupForm.get('password')!;
+  }
+
+  onSubmit() {
+    if (this.signupForm.valid) {
+      console.log(`entrou`);
+      const user = this.signupForm.value;
+      this.storageService.saveUser(user);
+      this.router.navigate(['/home']);
+      setTimeout(() => {
+        location.reload();
+      }, 1000);
+    }
+  }
+}
